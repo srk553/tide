ae0ef9769092b991004d38aa56531cde002583ef
srk553 <96103382+srk553@users.noreply.github.com>
2025-06-14 11:53:12 +0530

    Update lab11_bgp_assertion.py

--- lab11_bgp_assertion.py
+++ lab11_bgp_assertion.py
@@ -1,4 +1,3 @@
-root@Ansible:/Docker-images# cat lab15.yaml
 - name: Verify BGP neighbors before pushing changes
   hosts: csr1
   gather_facts: no
@@ -20,4 +19,3 @@ root@Ansible:/Docker-images# cat lab15.yaml
           - "'10.1.1.2' in bgp_output.stdout[0]"
           - "'Estab' in bgp_output.stdout[0] or 'Established' in bgp_output.stdout[0]"
         fail_msg: "BGP neighbor 10.1.1.2 is not in an Established state!"
-root@Ansible:/Docker-images#
