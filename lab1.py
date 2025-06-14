e21ccb1d690d658952f8ae88e04a8f8ed2ef0bf4
srk553 <96103382+srk553@users.noreply.github.com>
2025-06-14 10:41:10 +0530

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,20 @@
+
+
+from netmiko import ConnectHandler
+
+device = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.1.1',
+    'username': 'admin',
+    'password': 'lab123',
+    'secret': 'lab123'
+}
+
+net_connect = ConnectHandler(**device)
+net_connect.enable()
+
+output = net_connect.send_command("show ip int brief")
+print(output)
+
+net_connect.disconnect()
+
